4ff0efbbdb47019c149fc7493167a5f3748b0056
ersok <ersokmate98@gmail.com>
2020-02-12 12:14:19 +0100

    dir

--- urlmanage.sh
+++ urlmanage.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 sudo apt-get --purge remove firefox -y
-sudo apt update apt upgrade -y
+sudo apt update && apt upgrade -y
 
 DIR="/etc/chromium-browser/policies/managed" 
-if [ -e $DIR] 
-then
-    echo "juhhhu"
-fi
 
-sudo mkdir /etc/chromium-browser/policies/managed
+sudo mkdir $DIR
 
 
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 
-sudo cp $BASEDIR/URL* /etc/chromium-browser/policies/managed
+sudo cp $BASEDIR/URL* $DIR
 echo "Kész"
 
 read -r "Press enter to continue"
