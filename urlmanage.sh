b706eaebc3e3ff3e672b40cdba4ae01258759a27
ersok <ersokmate98@gmail.com>
2020-02-12 11:28:12 +0100

    create rule

--- urlmanage.sh
+++ urlmanage.sh
@@ -1,2 +1,21 @@
+#!/bin/sh
 sudo apt-get --purge remove firefox -y
-sudo apt update
+sudo apt update apt upgrade -y
+sudo mkdir /etc/chromium-browser/policies/managed
+"
+{
+    "URLBlacklist":
+    ["*"]
+}
+
+" > /etc/chromium-browser/policies/managed/URLBlacklist.json
+echo "Blacklist created"
+{
+    "URLWhitelist":
+    [
+        "google.com",
+        "scoolcode.com"
+    ]
+} > /etc/chromium-browser/policies/managed/URLWhitelist.json
+
+exit 0
