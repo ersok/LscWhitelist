7d476d8a984bc6959073853521db842bbc642000
ersok <ersokmate98@gmail.com>
2020-02-12 11:48:56 +0100

    cp

--- urlmanage.sh
+++ urlmanage.sh
@@ -6,6 +6,8 @@ sudo mkdir /etc/chromium-browser/policies/managed
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 
+cp $BASEDIR/URL* /etc/chromium-browser/policies/managed
+
 read -p "Press enter to continue"
 
 
