6be79f6faad4557754ce5a4f65fde5e4be5b5b7c
ersokmate <ersokmate98@gmail.com>
2020-02-27 17:15:18 +0100

    Create curl download

--- whitelistupdater.sh
+++ whitelistupdater.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 sudo apt-get --purge remove firefox -y
-sudo apt update && apt upgrade -y
+#sudo apt update && apt upgrade -y
 
 DIR="/etc/chromium-browser/policies/managed" 
+curl -JLO https://raw.githubusercontent.com/ersok/LscWhitelist/master/URLWhitelist.json
 
 sudo mkdir $DIR
+sudo cp URLWhitelist.json $DIR
 
+#BASEDIR=$(dirname "$0")
+#echo "$BASEDIR"
 
-BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
-
-sudo cp $BASEDIR/URL* $DIR
+#sudo cp $BASEDIR/URL* $DIR
 clear
 echo "URL WHITELIST UPDATDED!"
 echo "Press enter to exit"
